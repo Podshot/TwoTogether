e10b255165e666f1189809bf219c08ff7ebda9a2
Podshot <ben.gothard3@gmail.com>
2015-08-27 18:44:39 -0400

    Forgot to commit a file's documentation

--- Assets/Scripts/LevelEditor/TransformData.cs
+++ Assets/Scripts/LevelEditor/TransformData.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Allows easy access to needed Transform data
 public class TransformData : MonoBehaviour {
 
 	public string type;
 
+    // Gets the position of the Transform
 	public Vector3 GetPosition() {
 		return GetComponent<Transform>().position;
 	}
 
+    // Gets the scale of the Transform
 	public Vector3 GetScale() {
 		return GetComponent<Transform>().lossyScale;
 	}
 
+    // Gets the type of terrain object the GameObject is
 	public string GetType() {
 		return type.ToUpper();
 	}

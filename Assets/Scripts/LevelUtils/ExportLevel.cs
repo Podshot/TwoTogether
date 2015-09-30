0ac2215a1af19861460aaccbd519a6af79c8003e
Podshot <ben.gothard3@gmail.com>
2015-09-29 20:52:13 -0400

    Started work on a LoadLevel script

--- Assets/Scripts/LevelUtils/ExportLevel.cs
+++ Assets/Scripts/LevelUtils/ExportLevel.cs
@@ -4,33 +4,40 @@ using System.Collections;
 
 public class ExportLevel : MonoBehaviour {
 
-    private GameObject spawnpoints;
-    private GameObject terrainParent;
-    private GameObject objectives;
+    public GameObject terrainParent;
+    public GameObject objectivesParent;
+    public GameObject spawnpointsParent;
+
     private TextWriter writer;
 
     void Start() {
-        writer = new StreamWriter("level_exported.json");
-        terrainParent = GameObject.FindGameObjectWithTag("Terrain");
-        spawnpoints = GameObject.FindGameObjectWithTag("SpawnController");
-        objectives = GameObject.FindGameObjectWithTag("Objectives");
+        writer = new StreamWriter(Application.loadedLevelName + ".json");
     }
 
+    #if UNITY_EDITOR
     void Update() {
         if (/*(Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftCommand)) &&*/ Input.GetKeyDown(KeyCode.E)) {
             Export();
         }
     }
+    #endif
 
     void Export() {
         Debug.Log("Exporting...");
+
         JSONObject level = new JSONObject(JSONObject.Type.OBJECT);
 
         JSONObject normalTerrain = new JSONObject(JSONObject.Type.ARRAY);
         JSONObject shadowTerrain = new JSONObject(JSONObject.Type.ARRAY);
 
+        JSONObject objectives = new JSONObject(JSONObject.Type.OBJECT);
+        JSONObject spawnpoints = new JSONObject(JSONObject.Type.OBJECT);
+
+        level.AddField("ID", Application.loadedLevelName);
         level.AddField("NormalTerrain", normalTerrain);
         level.AddField("ShadowTerrain", shadowTerrain);
+        level.AddField("Objectives", objectives);
+        level.AddField("Spawnpoints", spawnpoints);
 
         JSONObject addTo = null;
 
@@ -55,6 +62,7 @@ public class ExportLevel : MonoBehaviour {
             to.AddField("Scale", scale);
             to.AddField("Position", pos);
             to.AddField("Color", render.color.ToHexStringRGBA());
+
             // Debug field, can be removed when export testing is done
             to.AddField("Name", trans.name);
 
@@ -68,35 +76,70 @@ public class ExportLevel : MonoBehaviour {
 
             addTo.Add(to);
         }
-        foreach (Transform trans in spawnpoints.GetComponentInChildren<Transform>()) {
-            switch (trans.name.ToLower()) {
-                case "smallcubespawnpoint":
-                    JSONObject smallCubeObj = new JSONObject(JSONObject.Type.OBJECT);
-                    JSONObject smallCubePos = new JSONObject(JSONObject.Type.ARRAY);
 
-                    smallCubeObj.AddField("Position", smallCubePos);
-                    smallCubePos.Add(trans.position.x);
-                    smallCubePos.Add(trans.position.y);
-                    smallCubePos.Add(trans.position.z);
-
-                    level.AddField("SmallCubeSpawn", smallCubeObj);
+        foreach (SpriteRenderer renderer in objectivesParent.GetComponentsInChildren<SpriteRenderer>()) {
+            JSONObject obj = new JSONObject(JSONObject.Type.OBJECT);
+            switch (renderer.transform.name.ToLower()) {
+                case "bigcubeobjective":
+                    objectives.AddField("Blue", obj);
                     break;
-                case "bigcubespawnpoint":
-                    JSONObject bigCubeObj = new JSONObject(JSONObject.Type.OBJECT);
-                    JSONObject bigCubePos = new JSONObject(JSONObject.Type.ARRAY);
-
-                    bigCubeObj.AddField("Position", bigCubePos);
-                    bigCubePos.Add(trans.position.x);
-                    bigCubePos.Add(trans.position.y);
-                    bigCubePos.Add(trans.position.z);
-
-                    level.AddField("BigCubeSpawn", bigCubeObj);
+                case "smallcubeobjective":
+                    objectives.AddField("Red", obj);
                     break;
                 default:
                     break;
             }
+            Transform trans = renderer.transform;
+
+            JSONObject scale = new JSONObject(JSONObject.Type.ARRAY);
+            JSONObject pos = new JSONObject(JSONObject.Type.ARRAY);
+
+            obj.AddField("Scale", scale);
+            obj.AddField("Position", pos);
+            obj.AddField("Color", renderer.color.ToHexStringRGBA());
+
+            // Debug field, can be removed when export testing is done
+            obj.AddField("Name", trans.name);
+
+            scale.Add(trans.lossyScale.x);
+            scale.Add(trans.lossyScale.y);
+            scale.Add(trans.lossyScale.z);
+
+            pos.Add(trans.position.x);
+            pos.Add(trans.position.y);
+            pos.Add(trans.position.z);
         }
-        writer.WriteLine(level.ToString());
+
+        foreach (Transform trans in spawnpointsParent.GetComponentsInChildren<Transform>()) {
+            JSONObject point = new JSONObject(JSONObject.Type.OBJECT);
+            switch (trans.name.ToLower()) {
+                case "smallcubespawnpoint":
+                    spawnpoints.AddField("Red", point);
+                    break;
+                case "bigcubespawnpoint":
+                    spawnpoints.AddField("Blue", point);
+                    break;
+                default:
+                    break;
+            }
+            JSONObject scale = new JSONObject(JSONObject.Type.ARRAY);
+            JSONObject pos = new JSONObject(JSONObject.Type.ARRAY);
+
+            point.AddField("Scale", scale);
+            point.AddField("Position", pos);
+
+            // Debug field, can be removed when export testing is done
+            point.AddField("Name", trans.name);
+
+            scale.Add(trans.lossyScale.x);
+            scale.Add(trans.lossyScale.y);
+            scale.Add(trans.lossyScale.z);
+
+            pos.Add(trans.position.x);
+            pos.Add(trans.position.y);
+            pos.Add(trans.position.z);
+        }
+        writer.WriteLine(level.ToString(true));
         writer.Close();
         Debug.Log("Exported");
     }
---
+++ Assets/Scripts/LevelUtils/LoadLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LoadLevel : MonoBehaviour {
+
+    public GameObject terrainParent;
+    public GameObject objectiveParent;
+    public GameObject shadowParent;
+    public GameObject spawnpointsParent;
+    public GameObject normalTerrainPrefab;
+    public GameObject shadowTerrainPrefab;
+    public GameObject redKillerPrefab;
+    public GameObject blueKillerPrefab;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(Application.dataPath + "/Levels/" + "Level_1");
+        LoadLevelData("level_1");
+	}
+
+    private void LoadLevelData(string v) {
+        string levelData = System.IO.File.ReadAllText(Application.dataPath + "/Levels/" + v + ".json");
+        JSONObject level = new JSONObject(levelData);
+        Debug.Log(level["ID"].str);
+    }
+
+    private void RemoveOldLevel() {
+
+    }
+}

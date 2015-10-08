f5ff5944d361996a1f992688460ff7a0ec5ea50f
Podshot <ben.gothard3@gmail.com>
2015-10-08 18:55:35 -0400

    Tweaked the downloading script to point to an actual folder

--- Assets/Scripts/DownloadLevels.cs
+++ Assets/Scripts/DownloadLevels.cs
@@ -4,8 +4,8 @@ using System.IO;
 
 public class DownloadLevels : MonoBehaviour {
 
-    public const string BASEURL = "http://podshot.github.io/TwoTogether/";
-    //public const string BASE_URL = "http://127.0.0.1:8000/";
+    //public const string BASEURL = "http://podshot.github.io/TwoTogether/";
+    public const string BASEURL = "http://127.0.0.1:8000/";
 
     public IEnumerator Download() {
         WWW manifest = new WWW(BASEURL + "levels.manifest");
@@ -15,9 +15,11 @@ public class DownloadLevels : MonoBehaviour {
         for (int i = 0; i < levels.Count; i++) {
             bool shouldDownload = false;
             if (File.Exists(Application.dataPath + "/Levels/" + levels[i]["Name"].str)) {
-                if (!json[i]["Hash"].str.Equals(LevelManagementUtils.Hash(File.ReadAllText(Application.dataPath + "/Levels_/" + levels[i]["Name"].str)))) {
+                if (!json[i]["Hash"].str.Equals(LevelManagementUtils.Hash(File.ReadAllText(Application.dataPath + "/Levels/" + levels[i]["Name"].str)))) {
                     Debug.LogError("File hashes do not match for level \"" + levels[i]["Name"].str + "\"");
                     shouldDownload = true;
+                } else {
+                    Debug.Log("Level \"" + levels[i]["Name"].str + "\" already exists and matches Hash");
                 }
             } else {
                 shouldDownload = true;
@@ -34,7 +36,7 @@ public class DownloadLevels : MonoBehaviour {
 
                 File.WriteAllBytes(Application.dataPath + "/Levels/" + levels[i]["Name"].str, level.bytes);
 
-                if (!json[i]["Hash"].str.Equals(LevelManagementUtils.Hash(File.ReadAllText(Application.dataPath + "/Levels_/" + levels[i]["Name"].str)))) {
+                if (!json[i]["Hash"].str.Equals(LevelManagementUtils.Hash(File.ReadAllText(Application.dataPath + "/Levels/" + levels[i]["Name"].str)))) {
                     Debug.LogError("File hashes do not match for level \"" + levels[i]["Name"].str + "\"");
                 }
             }

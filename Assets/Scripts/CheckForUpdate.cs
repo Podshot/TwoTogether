4ff181b7409afba0964d6bd113d2a87aacb84482
Podshot <ben.gothard3@gmail.com>
2015-10-23 07:55:49 -0400

    The player can now open the update page by clicking the version text when it is red

--- Assets/Scripts/CheckForUpdate.cs
+++ Assets/Scripts/CheckForUpdate.cs
@@ -5,34 +5,24 @@ using System.Collections;
 // Checks for standalone updates
 public class CheckForUpdate : MonoBehaviour {
 
-	public string version;
-	public Text versionText;
 
-	private Text[] texts;
-	private Button updateButton;
+    private Text txt;
+    private string version;
 	private string url = "http://podshot.github.io/TwoTogether/version.txt";
 
 	void Awake() {
-		texts = GetComponentsInChildren<Text>();
-		updateButton = GetComponentInChildren<Button>();
-		versionText.text += version;
-        Debug.Log("Persistent: " + Application.persistentDataPath);
-        Debug.Log("Normal: " + Application.dataPath);
+        txt = GetComponent<Text>();
+        version = txt.text.Replace("Version: ", "");
+        StartCoroutine(Check());
 	}
 	
-	IEnumerator Start () {
+	IEnumerator Check() {
 		WWW www = new WWW(url);
 		yield return www;
 		if (www.text.Length>0 && !www.text.Equals(version)) {
-			foreach (Text text in texts) {
-				text.enabled = true;
-			}
-			updateButton.enabled = true;
-			updateButton.image.enabled = true;
+            txt.color = new Color(0.74902f, 0.06275f, 0.06275f);
+            GetComponent<Button>().enabled = true;
 		}
-	}
-
-	void OnMouseDown() {
-		Debug.Log("Clicked update text!");
+        yield break;
 	}
 }

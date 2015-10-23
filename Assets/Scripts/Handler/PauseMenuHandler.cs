92ecb9c5ef69152b4bd1ed151be6cbac0f769e31
Podshot <ben.gothard3@gmail.com>
2015-10-23 10:09:01 -0400

    Started work on the pause menu

---
+++ Assets/Scripts/Handler/PauseMenuHandler.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseMenuHandler : MonoBehaviour {
+
+    public Button[] buttons;
+    public Text txt;
+
+    // State
+    private bool inMenu =  false;
+    private int cooldown = 0;
+    private GameObject terrain;
+    private Text helpText;
+
+	// Use this for initialization
+	void Start () {
+        terrain = GameObject.FindGameObjectWithTag("Terrain");
+	}
+
+    IEnumerator FadeInMenu() {
+        yield break;
+    }
+
+    IEnumerator FadeOutMenu() {
+        yield break;
+    }
+
+    void ShowMenu() {
+        foreach (SpriteRenderer renderer in terrain.GetComponentsInChildren<SpriteRenderer>()) {
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 0.5f);
+        }
+        helpText.color = new Color(helpText.color.r, helpText.color.g, helpText.color.b, 0.5f);
+    }
+
+    void CloseMenu() {
+        foreach (SpriteRenderer renderer in terrain.GetComponentsInChildren<SpriteRenderer>()) {
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 1f);
+        }
+        helpText.color = new Color(helpText.color.r, helpText.color.g, helpText.color.b, 1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (helpText == null) {
+            helpText = Camera.main.GetComponent<GameState>().GetHelpText();
+        }
+        if (Input.GetKeyDown(KeyCode.P) && inMenu && cooldown == 0) {
+            CloseMenu();
+            cooldown = 20;
+            inMenu = false;
+        }
+        if (Input.GetKeyDown(KeyCode.P) && !inMenu && cooldown == 0) {
+            ShowMenu();
+            cooldown = 20;
+            inMenu = true;
+        }
+        if (cooldown > 0) {
+            cooldown--;
+        }
+	}
+}

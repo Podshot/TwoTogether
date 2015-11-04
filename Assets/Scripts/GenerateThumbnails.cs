c4cb05c0cd6ff26161cdb894388c80b534eea3e4
Podshot <ben.gothard3@gmail.com>
2015-11-03 21:12:13 -0500

    Mid stage commit. Dynamically generating thumbnails

--- Assets/Scripts/GenerateThumbnails.cs
+++ Assets/Scripts/GenerateThumbnails.cs
@@ -5,7 +5,11 @@ using System.IO;
 
 public class GenerateThumbnails : MonoBehaviour {
 
+    public GameObject prefab;
+    public GameObject paren;
     public Image unknown;
+
+    private float[][] points = new float[][] { new float[] { -200f, 125f }, new float[] { 200f, 125f }, new float[] { -200f, -145f}, new float[] { 200f, -145f } }; 
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +24,7 @@ public class GenerateThumbnails : MonoBehaviour {
                 Texture2D tex2d = new Texture2D(800, 600);
                 tex2d.LoadImage(File.ReadAllBytes(Application.dataPath + "/Level_Thumbnails/level_" + (i + 1) + ".png"));
                 image.sprite = Sprite.Create(tex2d, new Rect(0, 0, tex2d.width, tex2d.height), image.sprite.pivot);
+                image.enabled = true;
 
                 Text txt = image.GetComponentInChildren<Text>();
                 txt.text = "Level " + (i + 1);
@@ -32,6 +37,7 @@ public class GenerateThumbnails : MonoBehaviour {
                 Image image = images[i];
                 image.sprite = unknown.sprite;
                 image.color = unknown.color;
+                image.enabled = true;
 
                 Text txt = image.GetComponentInChildren<Text>();
                 txt.text = "?";
@@ -45,6 +51,6 @@ public class GenerateThumbnails : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }

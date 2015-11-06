5f23f57ad932bfc9021ef0c3409cebd38e7224ed
Podshot <ben.gothard3@gmail.com>
2015-11-06 07:48:13 -0500

    Fixed an issue where not having a thumbnail would throw an error

--- Assets/Scripts/GenerateThumbnails.cs
+++ Assets/Scripts/GenerateThumbnails.cs
@@ -51,8 +51,13 @@ public class GenerateThumbnails : MonoBehaviour {
             if ((i + 1) <= progress["Progress"].n) {
                 Image image = images[i];
                 Texture2D tex2d = new Texture2D(800, 600);
-                tex2d.LoadImage(File.ReadAllBytes(Application.dataPath + "/Level_Thumbnails/level_" + (i + 1) + ".png"));
-                image.sprite = Sprite.Create(tex2d, new Rect(0, 0, tex2d.width, tex2d.height), image.sprite.pivot);
+                try {
+                    tex2d.LoadImage(File.ReadAllBytes(Application.dataPath + "/Level_Thumbnails/level_" + (i + 1) + ".png"));
+                    image.sprite = Sprite.Create(tex2d, new Rect(0, 0, tex2d.width, tex2d.height), image.sprite.pivot);
+                } catch (FileNotFoundException e) {
+                    image.sprite = unknown.sprite;
+                    image.color = unknown.color;
+                }
                 image.enabled = true;
 
                 Text txt = image.GetComponentInChildren<Text>();
